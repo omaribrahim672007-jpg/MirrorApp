664c795ea8d12aba7eeba2e98fb4ff7ad3f8eeef
omigar <omaribrahim672007@gmail.com>
2026-05-06 09:54:59 +0200

    Create main.dart

---
+++ .github/workflows/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyWebView(),
+  ));
+}
+
+class MyWebView extends StatefulWidget {
+  const MyWebView({super.key});
+  @override
+  State<MyWebView> createState() => _MyWebViewState();
+}
+
+class _MyWebViewState extends State<MyWebView> {
+  late final WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse('https://preview--my-mirror-app.lovable.app/')); // رابطك هنا
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(child: WebViewWidget(controller: controller)),
+    );
+  }
+}
